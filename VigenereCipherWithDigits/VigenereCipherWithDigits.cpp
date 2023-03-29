47f06d06704dfa841ed644eedc1f955cac1f88ba
Arkadiusz Cieślak <UL0246800@edu.uni.lodz.pl>
2023-03-29 12:38:41 +0200

    Encription implemented

--- VigenereCipherWithDigits/VigenereCipherWithDigits.cpp
+++ VigenereCipherWithDigits/VigenereCipherWithDigits.cpp
@@ -1,20 +1,96 @@
 ﻿// VigenereCipherWithDigits.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
 //
 
+
+
+#include <fstream>
 #include <iostream>
+#include <string>
+const char Alphabet[] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9' };
+const int Alphabet_Size = sizeof(Alphabet) / sizeof(Alphabet[0]);
+using namespace std;
+
+int position(char character);
+
+string Vigenere_cipher(string text, string key);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int decision;
+    
+    string key, text;
+
+    ifstream input_file("input.txt");
+    if (!input_file) {
+        cout << "Failed to open input file." << endl;
+        return 1;
+    }
+
+    ofstream output_file("encrypted.txt");
+    if (!output_file) {
+        cout << "Failed to open encrypted file." << endl;
+        return 1;
+    }
+
+    cout << "Welcome to Vigenere Cipher\nDo you want to encrypt(1), decrypt(2)" << endl;
+    cin >> decision;
+    if (decision == 1)
+    {
+        cout << "You choose encryption\nEnter the key of encription:";
+        cin >> key;
+        while (getline(input_file, text)) {
+
+            string encrypted_line = Vigenere_cipher(text, key);
+            output_file << encrypted_line << endl;
+
+        }
+        text = "";
+    }
+    if (decision == 2)
+    {
+        cout << "You choose decryption\nEnter the key of decryption:";
+        cin >> key;
+        while (getline(input_file, text)) {
+
+            string encrypted_line = Vigenere_cipher(text, key);
+            output_file << encrypted_line << endl;
+
+        }
+        text = "";
+    }
+    else
+    {
+        return 0;
+    }
+
+
+
+    input_file.close();
+    output_file.close();
+
+    return 0;
+
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+int position(char character)
+{
+    for (int i = 0; i < Alphabet_Size; i++)
+    {
+        if (Alphabet[i] == character)
+        {
+            return i;
+        }
+    }
+}
+string Vigenere_cipher(string text, string key)
+{
+    string result = "";
+    for (int i = 0; i < text.size(); i++)
+    {
+        int positionKey = position(key[i % key.size()]);
+        int positionText = position(text[i]);
+        result += Alphabet[(positionText + positionKey) % Alphabet_Size];
+    }
+    return result;
+}
 
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
